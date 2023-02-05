303f5eadea35e96507589c72b7a728ddcb77835a
Jun Hyeop <limjunehyeop@gmail.com>
2023-02-05 22:18:59 +0900

    fake commit

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ fakePath = os.path.dirname(os.path.realpath(__file__))
 fakeFile = open(fakePath+'/fakeFile/fakeFile.txt', 'r')
 fakeData = fakeFile.readline()
 fakeFile.close()
-fakeFile = open(fakePath+'/fakeFile/fakeFile.txt', 'a')
 print(fakeData)
 while num < int(times):
+ fakeFile = open(fakePath+'/fakeFile/fakeFile.txt', 'a')
  fakeFile.write('1')
 
  os.chdir(fakePath)
@@ -16,3 +16,4 @@ while num < int(times):
  os.system('git commit -m "fake commit"')
  os.system('git push')
  num += 1
+ fakeFile.close()
