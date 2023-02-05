f7129682ff1994ca18896453303e44939c2a1147
Jun Hyeop <limjunehyeop@gmail.com>
2023-02-05 09:50:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import os
-
+os.chdir('/')
+fakePath = os.path.dirname(os.path.realpath(__file__)) )
+fakeFile = open(fakePath+'/fakeFile/fakeFile.txt', 'r')
+fakeData = fakeFile.readline()
+fakeFile.close()
+fakeFile = open(fakePath+'/fakeFile/fakeFile.txt', 'r')
+try:
+ fakeFile.write(Int(fakeData)+1)
+except:
+ print('e')
+ fakeFile.write(1)
+os.system('git add .')
+os.system('git commit')
+os.system('git push')
